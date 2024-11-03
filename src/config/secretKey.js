0af4d6ff0540f36774535a1c204734c15bcbe561
Riquelme <riquelmestayler@gmail.com>
2024-11-02 23:39:10 -0300

    deleted arquives

--- src/config/secretKey.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.secretKey = void 0;
-exports.secretKey = process.env.SECRETKEY;
-if (!exports.secretKey) {
-    throw new Error('Not existing Secret Key');
-}
--- src/validation/categorySchema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.categorySchema = void 0;
-const zod_1 = require("zod");
-exports.categorySchema = zod_1.z.object({
-    category: zod_1.z.string({ message: 'Mande a categoria' }).min(2, { message: 'Categoria deve conter no minimo 2 caracteres' })
-});
